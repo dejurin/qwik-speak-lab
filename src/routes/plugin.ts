08bb896499b2849d74ef45f4d2cb10f59679b402
YURII D <gkhelloworld@gmail.com>
2024-02-22 00:33:44 +0200

    Update plugin.ts

--- src/routes/plugin.ts
+++ src/routes/plugin.ts
@@ -1,18 +1,35 @@
 import type { RequestHandler } from '@builder.io/qwik-city';
-import { validateLocale } from 'qwik-speak';
+import type { RewriteRouteOption} from 'qwik-speak';
+import { extractFromDomain, extractFromUrl, validateLocale } from 'qwik-speak';
 
 import { config } from '../speak-config';
+export const rewriteRoutes: RewriteRouteOption[] = [
+    // No prefix/paths for default locale
+    {
+      domain: 'emoj.info',
+      paths: {}
+    },
+    {
+      prefix: 'it',
+      domain: 'it.emoj.info',
+      paths: {}
 
-export const onRequest: RequestHandler = ({ params, locale, error }) => {
+    }
+  ];
+
+export const onRequest: RequestHandler = ({ locale, error, url }) => {
   let lang: string | undefined = undefined;
 
-  if (params.lang && validateLocale(params.lang)) {
+  const prefix = extractFromUrl(url);
+
+  if (prefix && validateLocale(prefix)) {
     // Check supported locales
-    lang = config.supportedLocales.find(value => value.lang === params.lang)?.lang;
+    lang = config.supportedLocales.find(value => value.lang === prefix)?.lang;
     // 404 error page
     if (!lang) throw error(404, 'Page not found');
   } else {
-    lang = config.defaultLocale.lang;
+    // Extract from domain
+    lang = extractFromDomain(url, rewriteRoutes) || config.defaultLocale.lang;
   }
 
   // Set Qwik locale

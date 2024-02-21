83cffa476f484b59bc7b8a81b4c84e4231cc41c9
YURII D <gkhelloworld@gmail.com>
2024-02-22 00:42:33 +0200

    up

--- src/speak-config.ts
+++ src/speak-config.ts
@@ -1,20 +1,20 @@
 import type { SpeakConfig } from 'qwik-speak';
 
 export const config: SpeakConfig = {
-  defaultLocale: { lang: 'en-US', currency: 'USD', timeZone: 'America/Los_Angeles' },
-  supportedLocales: [
-    { lang: 'it-IT', currency: 'EUR', timeZone: 'Europe/Rome' },
-    { lang: 'en-US', currency: 'USD', timeZone: 'America/Los_Angeles' }
-  ],
-  domainBasedRouting: {
-    prefix: 'as-needed'
-  },
-  // Translations available in the whole app
-  assets: [
-    'app'
-  ],
-  // Translations with dynamic keys available in the whole app
-  runtimeAssets: [
-    'runtime'
-  ]
+    defaultLocale: { lang: 'en-US', currency: 'USD', timeZone: 'America/Los_Angeles' },
+    supportedLocales: [
+        { lang: 'it-IT', currency: 'EUR', timeZone: 'Europe/Rome', domain: 'it-it.emoj.info'},
+        { lang: 'en-US', currency: 'USD', timeZone: 'America/Los_Angeles', domain: 'emoj.info'}
+    ],
+    domainBasedRouting: {
+        prefix: 'as-needed'
+    },
+    // Translations available in the whole app
+    assets: [
+        'app'
+    ],
+    // Translations with dynamic keys available in the whole app
+    runtimeAssets: [
+        'runtime'
+    ]
 };
